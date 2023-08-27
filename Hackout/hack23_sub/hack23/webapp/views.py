43897ce5aa54e69d2643f9ed5a96c5a0e4cb7855
RiddhiJayswal <114859883+RiddhiJayswal@users.noreply.github.com>
2023-08-27 10:21:23 +0530

    initial commit

---
+++ Hackout/hack23_sub/hack23/webapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+def home(request):
+    return render(request,"home.html")
+def home(request):
+    return render(request,"hospitals.html")
+def home(request):
+    return render(request,"index.html")
+def home(request):
+    return render(request,"info.html")
+def home(request):
+    return render(request,"patientsabha.html")
+def home(request):
+    return render(request,"register.html")
+def home(request):
+    return render(request,"specialists.html")
+
+# Create your views here.
